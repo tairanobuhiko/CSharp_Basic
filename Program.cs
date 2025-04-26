e5aaf7b28ee43f1968f1981752c76d17f27ee1ca
no215tyler <no.215.tyler@gmail.com>
2025-04-26 21:30:41 +0900

    【次元配列の入出力】i番目の出力 Boss C#編

--- Program.cs
+++ Program.cs
@@ -1,71 +1,14 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Security.Cryptography.X509Certificates;
-using System.Globalization;
-using System.Diagnostics;
 using System.Collections;
 
 class Program
 {
     static void Main()
     {
-        string[] input = Console.ReadLine().Split(" ");
-        int N = int.Parse(input[0]);
-        int Q = int.Parse(input[1]);
-        List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
-
-        // クエリ処理（Q回）
-        for (int i = 0; i < Q; i++)
-        {
-            int[] query = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
-            switch (query[0])
-            {
-                case 0:
-                    PushBack(query[1], nums);
-                    break;
-
-                case 1:
-                    PopBack(nums);
-                    break;
-
-                case 2:
-                    Print(nums);
-                    break;
-
-                default:
-                    break;
-            }
-        }
+        int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+        int[] nums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+        Console.WriteLine(nums[input[1] - 1]);
     }
-
-
-    private static void PushBack(int x, List<int> nums)
-    {
-        nums.Add(x);
-    }
-
-    private static void PopBack(List<int> nums)
-    {
-        int index = nums.Count - 1;
-        nums.RemoveAt(index);
-    }
-
-    private static void Print(List<int> nums)
-    {
-        int lastIndex = nums.Count - 1;
-        for (int i = 0; i <= lastIndex; i++)
-        {
-            if (i != lastIndex)
-            {
-                Console.Write($"{nums[i]} ");
-            }
-            else
-            {
-                Console.WriteLine(nums[i]);
-            }
-        }
-    }
-
 }
