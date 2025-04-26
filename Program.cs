2d62818a613bf6b621894066b312bf3200204519
no215tyler <no.215.tyler@gmail.com>
2025-04-26 23:09:55 +0900

    【配列の検索】何個ある? Boss C#編

--- Program.cs
+++ Program.cs
@@ -10,17 +10,9 @@ class Program
         int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
         int N = input[0];
         int M = input[1];
-        int K = input[2] - 1;
-        int L = input[3] - 1;
 
-        List<List<int>> dimension2 = new();
-
-        for (int i = 0; i < N; i++)
-        {
-            List<int> dimension1 = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
-            dimension2.Add(dimension1);
-        }
-
-        Console.WriteLine(dimension2[K][L]);
+        int[] nums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+        List<int> result = nums.Where(n => n == N).ToList();
+        Console.WriteLine(result.Count);
     }
 }
