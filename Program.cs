1d0f1d0a0639a548e6016349c5685b6c040a1d08
no215tyler <no.215.tyler@gmail.com>
2025-04-26 21:41:20 +0900

    【二次元配列の入出力】i番目の出力 Boss C#編

--- Program.cs
+++ Program.cs
@@ -8,7 +8,19 @@ class Program
     static void Main()
     {
         int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
-        int[] nums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
-        Console.WriteLine(nums[input[1] - 1]);
+        int N = input[0];
+        int M = input[1];
+        int K = input[2] - 1;
+        int L = input[3] - 1;
+
+        List<List<int>> dimension2 = new();
+
+        for (int i = 0; i < N; i++)
+        {
+            List<int> dimension1 = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            dimension2.Add(dimension1);
+        }
+
+        Console.WriteLine(dimension2[K][L]);
     }
 }
