42d5fbee34126fed4a327d1539279286d50a2759
no215tyler <no.215.tyler@gmail.com>
2025-04-26 23:49:32 +0900

    指定の要素のカウント C#編

--- Program.cs
+++ Program.cs
@@ -8,11 +8,16 @@ class Program
     static void Main()
     {
         List<int> input = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+        int N = input[0];
+        int K = input[1];
 
-        List<int> result = input.Distinct().OrderBy(x => x).ToList();
-        foreach (int num in result)
+        List<int> nums = new List<int>();
+        for (int i = 0; i < N; i++)
         {
-            Console.WriteLine(num);
+            nums.Add(int.Parse(Console.ReadLine()));
         }
+
+        int count = nums.Where(n => n == K).Count();
+        Console.WriteLine(count);
     }
 }
