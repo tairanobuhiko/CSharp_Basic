3f2bcd092ced1144026cfc83e301a328863b2cc2
no215tyler <no.215.tyler@gmail.com>
2025-04-29 01:03:58 +0900

    switch式の練習

--- Program.cs
+++ Program.cs
@@ -1,36 +1,20 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Collections;
-using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main()
     {
-        int N = int.Parse(Console.ReadLine());
-        List<Employee> employees = new List<Employee>();
-        for (int i = 0; i < N; i++)
-        {
-            string[] input = Console.ReadLine().Split(" ").ToArray();
-            employees.Add(new Employee(input[0], int.Parse(input[1])));
-        }
+        int month = int.Parse(Console.ReadLine());
 
-        foreach (Employee employee in employees)
-        {
-            employee.Age += 1;
-            Console.WriteLine($"{employee.Name} {employee.Age}");
-        }
-    }
 
-    class Employee
-    {
-        public string Name { get; set; } = string.Empty;
-        public int Age { get; set; } = 0;
-        public Employee(string name, int age)
+        string season = month switch
         {
-            this.Name = name;
-            this.Age = age;
-        }
+            (>= 3 and <= 5) => "春",
+            (>= 6 and <= 8) => "夏",
+            (>= 9 and <= 11) => "秋",
+            (12 or 1 or 2) => "冬",
+            _ => "不正な月"
+        };
+        Console.WriteLine($"{month}月は{season}です");
     }
 }
