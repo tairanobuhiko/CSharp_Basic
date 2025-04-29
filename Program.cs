ff8b0dfea51d9f0a6094156a93c0a0d456815a99
no215tyler <no.215.tyler@gmail.com>
2025-04-29 18:33:08 +0900

    先頭の要素の削除(query) C#編

--- Program.cs
+++ Program.cs
@@ -6,17 +6,50 @@ class Program
 {
     static void Main()
     {
-        List<int> input = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
-        List<int> list = new List<int>();
+        int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+        List<int> nums = new List<int>();
         for (int i = 0; i < input[0]; i++)
         {
-            list.Add(int.Parse(Console.ReadLine()));
+            nums.Add(int.Parse(Console.ReadLine()));
         }
-        list.Insert(input[1], input[2]);
 
-        foreach (int num in list)
+        // query
+        for (int i = 0; i < input[1]; i++)
         {
-            Console.WriteLine(num);
+            var query = Console.ReadLine();
+            List<int> result = query switch
+            {
+                "pop" => Pop(nums),
+                "show" => Show(nums),
+                _ => nums
+            };
         }
+    }
+
+    /// <summary>
+    /// A の先頭の要素を削除する。既に A に要素が存在しない場合何もしない。
+    /// </summary>
+    private static List<int> Pop(List<int> nums)
+    {
+        if (nums.Count > 0)
+        {
+            nums.RemoveAt(0);
+        }
+        return nums;
+    }
+
+    /// <summary>
+    /// A の要素を先頭から順に改行区切りで出力する。A に要素が存在しない場合何も出力しない。
+    /// </summary>
+    private static List<int> Show(List<int> nums)
+    {
+        if (nums.Count > 0)
+        {
+            foreach (int num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        return nums;
     }
 }
