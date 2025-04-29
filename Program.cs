e1e7b1e93482e5ce9ff246936e7f74dbe00f4260
no215tyler <no.215.tyler@gmail.com>
2025-04-29 21:50:02 +0900

    【行入力】1,000 行の入力 C#編

--- Program.cs
+++ Program.cs
@@ -1,31 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        int N = int.Parse(Console.ReadLine());
-
-        List<string> list = new List<string>();
-        for (int i = 0; i < N; i++)
+        string input;
+        while ((input = Console.ReadLine()) != null)
         {
-            SearchHistory(list, Console.ReadLine());
+            Console.WriteLine(input);
         }
-        foreach (string word in list)
-        {
-            Console.WriteLine(word);
-        }
-    }
-
-    private static List<string> SearchHistory(List<string> list, string word)
-    {
-        int index = list.IndexOf(word);
-        if (index >= 0)
-        {
-            list.RemoveAt(index);
-        }
-        list.Insert(0, word);
-        return list;
     }
 }
