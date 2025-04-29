b43d8d511101f176b865c6d66af003b1b7a3867c
no215tyler <no.215.tyler@gmail.com>
2025-04-29 21:33:03 +0900

    検索履歴 C#編

--- Program.cs
+++ Program.cs
@@ -1,19 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        for (int i = 1; i <= 100; i++)
+        int N = int.Parse(Console.ReadLine());
+
+        List<string> list = new List<string>();
+        for (int i = 0; i < N; i++)
         {
-            string result = (i % 3, i % 5) switch
-            {
-                (0, 0) => "Fizz Buzz",
-                (0, _) => "Fizz",
-                (_, 0) => "Buzz",
-                _ => i.ToString()
-            };
-            Console.WriteLine(result);
+            SearchHistory(list, Console.ReadLine());
         }
+        foreach (string word in list)
+        {
+            Console.WriteLine(word);
+        }
+    }
+
+    private static List<string> SearchHistory(List<string> list, string word)
+    {
+        int index = list.IndexOf(word);
+        if (index >= 0)
+        {
+            list.RemoveAt(index);
+        }
+        list.Insert(0, word);
+        return list;
     }
 }
