80fd40297dee0f90891a66c1d297fa4c760bceb1
no215tyler <no.215.tyler@gmail.com>
2025-04-29 21:08:04 +0900

    FizzBuzz: switch文

--- Program.cs
+++ Program.cs
@@ -1,55 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class Program
 {
     static void Main()
     {
-        int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
-        List<int> nums = new List<int>();
-        for (int i = 0; i < input[0]; i++)
+        for (int i = 1; i <= 100; i++)
         {
-            nums.Add(int.Parse(Console.ReadLine()));
-        }
-
-        // query
-        for (int i = 0; i < input[1]; i++)
-        {
-            var query = Console.ReadLine();
-            List<int> result = query switch
+            switch (i)
             {
-                "pop" => Pop(nums),
-                "show" => Show(nums),
-                _ => nums
-            };
-        }
-    }
+                case int _ when i % 15 == 0:
+                    Console.WriteLine("Fizz Buzz");
+                    break;
 
-    /// <summary>
-    /// A の先頭の要素を削除する。既に A に要素が存在しない場合何もしない。
-    /// </summary>
-    private static List<int> Pop(List<int> nums)
-    {
-        if (nums.Count > 0)
-        {
-            nums.RemoveAt(0);
-        }
-        return nums;
-    }
+                case int _ when i % 3 == 0:
+                    Console.WriteLine("Fizz");
+                    break;
 
-    /// <summary>
-    /// A の要素を先頭から順に改行区切りで出力する。A に要素が存在しない場合何も出力しない。
-    /// </summary>
-    private static List<int> Show(List<int> nums)
-    {
-        if (nums.Count > 0)
-        {
-            foreach (int num in nums)
-            {
-                Console.WriteLine(num);
+                case int _ when i % 5 == 0:
+                    Console.WriteLine("Buzz");
+                    break;
+
+                default:
+                    Console.WriteLine(i);
+                    break;
             }
         }
-        return nums;
     }
 }
