1db7e05aaac773a6b0509287d62534f90920b424
no215tyler <no.215.tyler@gmail.com>
2025-04-30 20:57:13 +0900

    【N 行 M 列のデータの入力】行ごとに要素数の異なる整数列の入力 C#編

--- Program.cs
+++ Program.cs
@@ -1,13 +1,27 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 class Program
 {
     static void Main()
     {
+        int N = int.Parse(Console.ReadLine());
         string input;
         while ((input = Console.ReadLine()) != null)
         {
-            Console.WriteLine(input);
+            List<int> nums = input.Split(" ").Select(int.Parse).ToList();
+            int lastIndex = nums.Count - 1;
+            for (int i = 1; i <= lastIndex; i++)
+            {
+                if (i != lastIndex)
+                {
+                    Console.Write($"{nums[i]} ");
+                }
+                else
+                {
+                    Console.WriteLine(nums[i]);
+                }
+            }
         }
     }
 }
