9d60f9318e8bdf0bf066372f478def8af1a39c59
no215tyler <no.215.tyler@gmail.com>
2025-05-02 21:58:41 +0900

    5以上の整数の合計 C#編

--- Program.cs
+++ Program.cs
@@ -5,46 +5,15 @@ class Program
 {
     static void Main()
     {
-        int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
-        int N = input[0];
-        int K = input[1];
-        int P = input[2];
-
-        List<int> students = new List<int> { P };
+        int N = int.Parse(Console.ReadLine());
+        int result = 0;
         for (int i = 0; i < N; i++)
         {
-            students.Add(int.Parse(Console.ReadLine()));
+            int num = int.Parse(Console.ReadLine());
+            result += num >= 5
+                ? num
+                : 0;
         }
-
-        for (int i = 0; i < K; i++)
-        {
-            string[] query = Console.ReadLine().Split(" ");
-            switch (query[0])
-            {
-                case "join":
-                    Join(students, int.Parse(query[1]));
-                    break;
-
-                case "sorting":
-                    Console.WriteLine(Sorting(students, P));
-                    break;
-            }
-        }
-
-    }
-
-    private static void Join(List<int> students, int height)
-    {
-        students.Add(height);
-    }
-
-    private static int Sorting(List<int> students, int P)
-    {
-        int count = 0;
-        foreach (var h in students)
-        {
-            if (h < P) count++;
-        }
-        return count + 1;
+        Console.WriteLine(result);
     }
 }
