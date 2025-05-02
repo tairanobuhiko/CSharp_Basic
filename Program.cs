029ebe11fbc479e265e21dfa0b87a3b5163370ca
no215tyler <no.215.tyler@gmail.com>
2025-05-02 23:39:03 +0900

    足すか掛けるか C#編

--- Program.cs
+++ Program.cs
@@ -9,10 +9,11 @@ class Program
         int result = 0;
         for (int i = 0; i < N; i++)
         {
-            int num = int.Parse(Console.ReadLine());
-            result += num >= 5
-                ? num
-                : 0;
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int calcResult = input[0] == input[1]
+                ? input[0] * input[1]
+                : input[0] + input[1];
+            result += calcResult;
         }
         Console.WriteLine(result);
     }
