dccd0e629901fc4516c45cf22fa2f7f8310a6168
no215tyler <no.215.tyler@gmail.com>
2025-05-09 01:43:50 +0900

    文字と整数の組のソート C#編（タプルを使用）

--- Program.cs
+++ Program.cs
@@ -5,35 +5,18 @@ class Program
 {
     static void Main()
     {
-        int[] nums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
-        int a = nums[0] - 1;
-        int b = nums[1] - 1;
-        List<string> refferenceNums = new List<string>();
-        for (int i = a; i <= b; i++)
+        int N = int.Parse(Console.ReadLine());
+        List<(string a, int n)> list = new List<(string, int)>();
+        for (int i = 0; i < N; i++)
         {
-            refferenceNums.Add(i.ToString());
+            string[] input = Console.ReadLine().Split(" ");
+            list.Add((input[0], int.Parse(input[1])));
         }
 
-        string[] input = Console.ReadLine().Select(s => s.ToString()).ToArray();
-        List<string> result = new List<string>();
-        try
+        List<(string, int)> result = list.OrderBy(item => item.n).ToList();
+        foreach ((string a, int num) tuple in result)
         {
-            for (int i = 0; i < input.Length; i++)
-            {
-                if (refferenceNums.Contains(i.ToString()))
-                {
-                    result.Add(input[i].ToUpper());
-                }
-                else
-                {
-                    result.Add(input[i]);
-                }
-            }
-            Console.WriteLine(string.Join("", result));
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
+            Console.WriteLine(tuple.a);
         }
     }
 }
