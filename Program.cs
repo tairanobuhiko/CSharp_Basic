4c8ba0c7343996378ba5f5c3c08487f05cc89cb6
no215tyler <no.215.tyler@gmail.com>
2025-05-10 21:59:25 +0900

    文字と整数の組のソート2 C#編

--- Program.cs
+++ Program.cs
@@ -1,66 +1,26 @@
 ﻿using System;
-using System.Net.Http;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-
-// 投稿データのモデル（JSONと対応）
-public class Post
-{
-    [JsonPropertyName("userId")]
-    public int UserId { get; set; }
-
-    [JsonPropertyName("id")]
-    public int Id { get; set; }
-
-    [JsonPropertyName("title")]
-    public string Title { get; set; }
-
-    [JsonPropertyName("body")]
-    public string Body { get; set; }
-}
-
+using System.Collections.Generic;
+using System.Linq;
 class Program
 {
-    static async Task Main(string[] args)
+    static void Main()
     {
-        // HttpClientのインスタンスを作成
-        using HttpClient client = new HttpClient();
-
-        try
+        int N = int.Parse(Console.ReadLine());
+        Dictionary<string, int> data = new Dictionary<string, int>();
+        for (int i = 0; i < N; i++)
         {
-            // ① APIにGETリクエストを送信
-            HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/posts/");
+            string[] input = Console.ReadLine().Split(" ");
+            string key = input[0];
+            int value = int.Parse(input[1]);
 
-            // ② ステータスコードが成功かどうか確認
-            response.EnsureSuccessStatusCode();
-
-            // ③ レスポンスの本文（JSON）を取得
-            string responseBody = await response.Content.ReadAsStringAsync();
-
-            // ④ JSONをC#オブジェクトに変換してリストで受け取る
-            List<Post>? posts = JsonSerializer.Deserialize<List<Post>>(responseBody);
-
-            // ⑤ 特定のユーザーIDのみ抽出（Linq）
-            List<Post>? filterPosts = posts
-                .Where(p => p.UserId == 5)
-                .ToList();
-
-            // ⑥ 結果を表示(5件だけ)
-            if (filterPosts != null)
-            {
-                foreach (Post post in filterPosts)
-                {
-                    Console.WriteLine($"ユーザーID: {post.UserId}");
-                    Console.WriteLine($"投稿ID: {post.Id}");
-                    Console.WriteLine($"タイトル: {post.Title}");
-                    Console.WriteLine($"本文: {post.Body}");
-                }
-            }
+            data[key] = data.ContainsKey(key)
+                ? data[key] += value
+                : data[key] = value;
         }
-        catch (HttpRequestException e)
+
+        foreach (var pair in data.OrderByDescending(x => x.Value))
         {
-            Console.WriteLine($"エラー: {e.Message}");
+            Console.WriteLine($"{pair.Key} {pair.Value}");
         }
     }
 }
