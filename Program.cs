c99c61f3481a21a8783ebbabc0c446d1167d1498
no215tyler <no.215.tyler@gmail.com>
2025-05-11 12:13:37 +0900

    占い C#編

--- Program.cs
+++ Program.cs
@@ -6,21 +6,25 @@ class Program
     static void Main()
     {
         int N = int.Parse(Console.ReadLine());
-        Dictionary<string, int> data = new Dictionary<string, int>();
+        List<(string name, string chooseKey)> users = new List<(string, string)>();
         for (int i = 0; i < N; i++)
         {
             string[] input = Console.ReadLine().Split(" ");
-            string key = input[0];
-            int value = int.Parse(input[1]);
-
-            data[key] = data.ContainsKey(key)
-                ? data[key] += value
-                : data[key] = value;
+            (string, string) tuple = (input[0], input[1]);
+            users.Add(tuple);
         }
 
-        foreach (var pair in data.OrderByDescending(x => x.Value))
+        int M = int.Parse(Console.ReadLine());
+        Dictionary<string, string> lists = new Dictionary<string, string>();
+        for (int i = 0; i < M; i++)
         {
-            Console.WriteLine($"{pair.Key} {pair.Value}");
+            string[] input = Console.ReadLine().Split(" ");
+            lists.Add(input[0], input[1]);
+        }
+
+        foreach (var user in users)
+        {
+            Console.WriteLine($"{user.name} {lists[user.chooseKey]}");
         }
     }
 }
