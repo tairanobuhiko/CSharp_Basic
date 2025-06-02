8c90575009c8813221746da8114b480cfae1c4f2
no215tyler <no.215.tyler@gmail.com>
2025-06-03 00:08:15 +0900

    【マップの扱い 2】マップの書き換え・縦横 C#編

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ class Program
             .ToList();
 
         int H = prompt[0];
+        int W = prompt[1];
 
         for (int i = 0; i < H; i++)
         {
@@ -29,6 +30,14 @@ class Program
         string checkChar = boards[changeIndex[0]][changeIndex[1]];
         boards[changeIndex[0]][changeIndex[1]] = ChangeCell(checkChar);
 
+        List<List<int>> targetCells = ChangeCrossCell(H - 1, W - 1, changeIndex[0], changeIndex[1]);
+        foreach (var cells in targetCells)
+        {
+            checkChar = string.Empty;
+            checkChar = boards[cells[0]][cells[1]];
+            boards[cells[0]][cells[1]] = ChangeCell(checkChar);
+        }
+
         foreach (var board in boards)
         {
             Console.WriteLine(String.Join("", board));
@@ -43,5 +52,23 @@ class Program
             "." => "#",
             _ => String.Empty
         };
+    }
+
+    static List<List<int>> ChangeCrossCell(int h, int w, int y, int x)
+    {
+        List<List<int>> list = new List<List<int>>();
+        // top
+        if (y > 0 && y <= h) list.Add(new List<int>() { y - 1, x });
+
+        // down
+        if (y < h) list.Add(new List<int>() { y + 1, x });
+
+        // left
+        if (x > 0 && x <= w) list.Add(new List<int>() { y, x - 1 });
+
+        // right
+        if (x < w) list.Add(new List<int>() { y, x + 1 });
+
+        return list;
     }
 }
